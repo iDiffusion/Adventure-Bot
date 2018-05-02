b6f81b8db41c32e7eed2eea975ce6a1b2c2131f6
Ikaika <iDiffusion@users.noreply.github.com>
2018-05-02 14:01:31 -0400

    Updates as of 5/2

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
 "use strict";
 
-// Import files
+/* Import files */
 var tmi = require("tmi.js");
+var schedule = require('node-schedule');
+var cmd = require("./command.js");
 var config = require("./config.json");
+var prompt = require("./prompt.json");
 
-// Configure the client
+/* Configure the client */
 var options = {
   options: {
     debug: true
@@ -20,45 +23,102 @@ var options = {
 }
 var client = new tmi.client(options);
 
-//Save varibbles
+/* Save variables */
 var adventureEnabled = false;
+var noNewComers = true;
+var totem = []; //TODO change to hashmap
+var currentPath = undefined;
+var previousPaths = [];
+var choice = [];
+//!goal | !teatime | !butt | !behead | !F | !rules | !discord | !lurk | !back | !violet | !jamie | !redkat
 
-// Connect the client to the server
+/* Connect the client to the server */
 client.connect();
 
-// Recieve messages on channel
-client.on("chat", function (channel, userstate, message, self) {
+/* Recieve messages on channel */
+client.on("chat", function (channel, user, message, self) {
   if (self) return;   // Don't listen to my own messages...
-  let args = message.split(" ");
-  switch(args[0].toLowerCase()){
+  if(!message.startsWith("!")) return;// Don't listen to non-commands
+  switch(message.trim().split(" ")[0].toLowerCase()){
     case "!adventure":
-      client.say(channel, "TheCheshireKat appears to you and asks if you'd like to go on an adventure, type `!explore` to see where she'll lead you");
-      adventureEnabled = true;
+    if(user.username != config.user_admin && user.username != config.programmer) break;
+    client.say(channel, getPath(prompt.story, "adventure").value);
+    adventureEnabled = true;
+    noNewComers = false;
+    totem = [];
+    currentPath = "explore";
+    previousPaths = [];
+    choice = [];
+    previousPaths.push("adventure");
     break;
+
+
     case "!explore":
-      if(!adventureEnabled) break;
-      client.whisper(userstate.username, "Lets explore");
+    if(!adventureEnabled || noNewComers) break;
+    if((user.username == config.user_admin || user.username == config.programmer) && message.trim().split(" ").length > 1) {
+      noNewComers = true;
+      previousPaths.push("explore");
+
+    }
+    else {
+      //TODO check if a user has enough key fragments
+      client.say(channel, getPath(prompt.story, "explore").value.replace("$user", user["display-name"]));
+      totem[user["user-id"]] = undefined ? "null" : totem[user["user-id"]];
+    }
+    break;
+
+
+    case "!status":
+    console.log("Adventure mode is " + (adventureEnabled ? "enabled": "disabled") + ".");
+    console.log((noNewComers ? "Not": "Now") + " accepting new players.");
+    console.log("Totem array: " + totem.filter(function( element ) { return element !== undefined}));
+    console.log("Current path: " + currentPath);
+    console.log("Previous paths: " + previousPaths.toString());
+    console.log("Current choices: " + choice.filter(function( element ) { return element !== undefined}));
+    break;
+
+
+    default:
+      if(currentPath == "explore"){
+        let choices = prompt.story.explore.connected;
+      }
     break;
   }
 });
 
-// Connected to server
+function Member(user, message){
+  this.type = user["user-type"];
+  this.id = user["user-id"];
+  this.username = user["username"];
+  this.mod = user["mod"];
+  this.subscriber = user["subscriber"];
+  this.totem = undefined;
+  this.choice = undefined;
+}
+
+function getPath(array, val){
+  for (var i = 0; i < array.length; i++) {
+    if(array[i].path == val) return array[i];
+  }
+}
+
+/* Connected to server */
 client.on("connected", function (address, port) {
- console.log("Connecting to " + address + " on port " + port);
+  console.log("Connecting to " + address + " on port " + port + "!");
 });
 
-// Got disconnected from server
+/* Got disconnected from server */
 client.on("disconnected", function (reason) {
-  console.log("Diconnected from server because " + reason);
+  console.log("Diconnected from server because " + reason + ".");
 });
 
-// Trying to reconneect to server
+/* Trying to reconneect to server */
 client.on("reconnect", function () {
-  console.log("Attemping to reconnect to server");
+  console.log("Attemping to reconnect to server.");
 });
 
-// Recieved a whisper
+/* Recieved a whisper */
 client.on("whisper", function (from, userstate, message, self) {
   if (self) return;   // Don't listen to my own messages...
-  // Handle interation.
+  client.whisper(userstate.username, "Please join my host's channel http://wwww.twitch.tv/thecheshirekat !");
 });
---
+++ command.js
@@ -0,0 +1,42 @@
+exports.takeKnife = function(){
+
+};
+
+exports.takeBook = function(){
+
+};
+
+exports.takeLocket = function(){
+
+};
+//----------------------------------
+exports.startAdventure = function(){
+
+};
+
+exports.explore = function(){
+
+};
+//-------------------------------------------------
+exports.left = function(){
+
+};
+
+exports.right = function(){
+
+};
+//-------------------------------------------------
+exports.fight = function(){
+
+};
+exports.run = function(){
+
+};
+//------------------------------------------------
+exports.madhatter = function(){
+
+};
+exports.madchhare = function(){
+
+};
+//-------------------------------------------------
