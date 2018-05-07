83efc8bef06bafc80afe1385462069de57f6e4cc
Ikaika <iDiffusion@users.noreply.github.com>
2018-05-07 00:49:05 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@
 
 /* Import files */
 var tmi = require("tmi.js");
-var schedule = require('node-schedule');
 var config = require("./config.json");
 var prompt = require("./prompt.json");
 
@@ -23,6 +22,7 @@ var options = {
 var client = new tmi.client(options);
 
 /* Save variables */
+var testing = true;
 var adventureEnabled = false;
 var noNewComers = true;
 var currentPath = undefined;
@@ -77,8 +77,8 @@ client.on("chat", function (channel, user, message, self) {
       if(user.username != config.host && user.username != "itsdiffusion") break;
       console.log("Adventure mode is " + (adventureEnabled ? "enabled": "disabled") + ".");
       console.log((noNewComers ? "Not": "Now") + " accepting new players.");
-      console.log("Current path: " + currentPath);
       console.log("Previous paths: " + previousPaths.toString());
+      console.log("Current path: " + currentPath);
       console.log("Users playing: ");
       console.log(players.forEach(mem => console.log(JSON.stringify(mem))));
     break;
@@ -87,20 +87,21 @@ client.on("chat", function (channel, user, message, self) {
     default:
       if(currentPath == undefined || currentPath == null) break;
       if(getPlayer(user) == undefined || getPlayer(user) == null) break;
-      if(currentPath == "explore" && message.startsWith("!")){
+      if(!message.startsWith("!")) break;
+      if(currentPath == "explore") {
         let totems = getPath(prompt.story, "explore").totems;
         let totem = message.trim().slice(1).split(" ")[0];
-        if(totems.includes(totem)){
+        if(totems.includes(totem) && getPlayer(user) != null){
           getPlayer(user).totem = totem;
-          client.whisper(user.username, getPath(prompt.story, totem).value);
+          if(testing) client.whisper(user.username, getPath(prompt.story, totem).value);
         }
       }
-      else if(message.startsWith("!") && getPath(prompt.story, currentPath).userPick == true){
+      else if(getPath(prompt.story, currentPath) != null && getPath(prompt.story, currentPath).userPick == true){
         let choices = getPath(prompt.story, currentPath).connected;
         let choice = message.trim().slice(1).split(" ")[0];
         if(choices.includes(choice)){
           getPlayer(user).choice = choice;
-          client.whisper(user.username, "You have chosen to vote for " + choice);
+          if(testing) client.whisper(user.username, "You have chosen to vote for " + choice);
         }
       }
     break;
@@ -112,6 +113,7 @@ function Player(user){
   this.type = user["user-type"];
   this.id = user["user-id"];
   this.username = user["username"];
+  this.displayname = user["display-name"];
   this.mod = user["mod"];
   this.subscriber = user["subscriber"];
   this.totem = undefined;
@@ -123,6 +125,7 @@ function getPath(array, val){
   for (var i = 0; i < array.length; i++) {
     if(array[i].path == val) return array[i];
   }
+  return null;
 }
 
 /* get player object */
@@ -130,6 +133,7 @@ function getPlayer(user){
   for (var i = 0; i < players.length; i++) {
     if(players[i].username = user.username) return players[i];
   }
+  return null;
 }
 
 /* check if players have the totem */
@@ -142,70 +146,90 @@ function hasTotem(totem){
 
 /* give or take keys */
 function modifyKeys(channel){
-  var runnable = getPath(prompt.story, currentPath).runnable
-  if(runnable == null) return;
-  for(var i = 0; i< players.length; i++) {
+  var runnable = getPath(prompt.story, currentPath).runnable;
+  if(runnable == undefined || runnable == null) return;
+  var msgToPost = "";
+  for(var i = 0; i < players.length; i++){
     let rand = Math.floor(Math.random() * 50) + 1;
-    if(runnable == "giveKeys"){
-      //TODO give keys to players (use timeout)
-      client.say(channel, rand + " key fragments have been given to " + players[i].username);
-      console.log(rand + " key fragments have been given to " + players[i].username);
+    if(testing == true) {
+      if(runnable == "giveKeys") {
+        msgToPost += (rand + " key fragments have been given to " + players[i].displayname);
+        console.log(rand + " key fragments have been given to " + players[i].displayname);
+      }
+      else if(runnable == "takeKeys") {
+        msgToPost += (rand + " key fragments have been taken from " + players[i].displayname);
+        console.log(rand + " key fragments have been taken from " + players[i].displayname);
+      }
+      msgToPost += (i == players.length - 1) ? " ." : " , ";
+      if(i == players.length - 1) {
+        client.say(channel, msgToPost);
+      }
     }
-    else if(runnable == "takeKeys"){
-      //TODO take keys from players (use timeout)
-      client.say(channel, rand + " key fragments have been taken to " + players[i].username);
-      console.log(rand + " key fragments have been taken from " + players[i].username);
+    else {
+      if(runnable == "giveKeys") {
+        // TODO give keys to players (use timeout)
+        // client.say(channel, `!addpoints ${player[i].displayname} ${rand}`);
+        console.log(rand + " key fragments have been given to " + players[i].displayname);
+      }
+      else if(runnable == "takeKeys") {
+        // TODO take keys to players (use timeout)
+        // client.say(channel, `!addpoints ${player[i].displayname} -${rand}`);
+        console.log(rand + " key fragments have been taken from " + players[i].displayname);
+      }
     }
   }
 }
 
 /* Travel along the path */
-function travelPath(channel){
+function travelPath(channel, time){
   if(currentPath == "end" || getPath(prompt.story, currentPath).connected.length == 0) return;
   var paths = getPath(prompt.story, currentPath).connected;
-  console.log("List of paths: " + paths);
+  console.log("\nPrevious Paths: " + previousPaths);
   console.log("Current Path: " + currentPath);
-  console.log("Previous Paths: " + previousPaths);
+  console.log("List of paths: " + paths);
   var wantedPath = null;
   if(getPath(prompt.story, currentPath).userPick){
     let choices = Array(paths.length).fill(0);
-    console.log(choices);
     for (var m = 0; m < players.length; m++) {
       let i = paths.indexOf(players[m].choice);
       if(i != -1) choices[i] += 1;
     }
-    console.log(choices);
     previousPaths.push(currentPath);
     var index = 0;
-    for(var i = 0; i< choices.length; i++){
+    for(var i = 0; i< choices.length; i++) {
       if(i == 0) index = 0;
       else if(choices[index] < choices[i]) index = i;
     }
     wantedPath = paths[index];
-    if(previousPaths.includes(wantedPath)) return travelPath(channel);
+    console.log("Chosen Path: " + wantedPath);
+    if(previousPaths.includes(wantedPath) && time < 5) return travelPath(channel, time + 1);
     currentPath = wantedPath;
   }
   else {
     let rand = Math.floor(Math.random() * paths.length);
     wantedPath = paths[rand];
+    console.log("Chosen Path: " + wantedPath);
     let wantedTotem = getPath(prompt.story, currentPath).required;
-    if(wantedTotem == null || hasTotem(wantedPath)){
-      if(previousPaths.includes(wantedPath)) return travelPath(channel);
-      previousPaths.push(currentPath);
-      currentPath = wantedPath;
-    }
-    else {
+    if(wantedTotem != null && wantedTotem != undefined && !hasTotem(wantedPath)) {
       currentPath = previousPaths[previousPaths.length - 1];
-      return travelPath(channel);
+      return travelPath(channel, time + 1);
     }
+    if(previousPaths.includes(wantedPath) && time < 5 && !previousPaths.includes("end")) return travelPath(channel, time + 1);
+    previousPaths.push(currentPath);
+    currentPath = wantedPath;
   }
-  if(currentPath == "end") return adventureEnabled = false;
-  if(getPath(prompt.story, currentPath).value != null) client.say(channel, getPath(prompt.story, currentPath).value);
+  if(currentPath == "end") {
+    console.log("NOTE: The end of the story has been reached.")
+    return adventureEnabled = false;
+  }
+  if(getPath(prompt.story, currentPath).value != null) {
+    client.say(channel, getPath(prompt.story, currentPath).value);
+  }
   modifyKeys(channel);
-  if(getPath(prompt.story, currentPath).userPick == false){
-    return travelPath(channel);
+  if(getPath(prompt.story, currentPath).userPick == false) {
+    return travelPath(channel, 0);
   }
-  else{
+  else {
     //TODO call travelPath after timeout
   }
 }
